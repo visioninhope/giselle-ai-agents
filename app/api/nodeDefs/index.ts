f8178b7bd4f1fb32bca3f784c4109ef4b19f7e2d
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-07-10 17:57:29 +0900

    add sendMail node

--- app/api/nodeDefs/index.ts
+++ app/api/nodeDefs/index.ts
@@ -1,8 +1,9 @@
 import invariant from "tiny-invariant";
 import * as findUser from "./findUser";
+import * as sendMail from "./sendMail";
 import type { NodeDef } from "./type";
 
-export const nodeDefs = [findUser] satisfies NodeDef[];
+export const nodeDefs = [findUser, sendMail] satisfies NodeDef[];
 export type NodeType = (typeof nodeDefs)[number]["key"];
 export const getNodeDef = (type: NodeType): NodeDef => {
 	const nodeDef = nodeDefs.find((def) => def.key === type);
