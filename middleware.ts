651d2987ba4a979dc4739e4a4f960294dcfc00b8
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-07-29 17:56:36 +0900

    wip

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import type { NextRequest } from "next/server";
+import { updateSession } from "./lib/supabase";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware((auth, request) => {
-	if (!isPublicRoute(request)) {
-		auth().protect();
-	}
-});
+// export default clerkMiddleware((auth, request) => {
+// 	if (!isPublicRoute(request)) {
+// 		auth().protect();
+// 	}
+// });
 
+export async function middleware(request: NextRequest) {
+	return await updateSession(request);
+}
 export const config = {
 	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
