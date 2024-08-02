cd457fc981962514ead00edb2e4fd4c444eefcf2
Gen Tamura <gen.tamura84@gmail.com>
2024-08-02 14:10:59 +0900

    Update shell script

--- ignore-build-step.sh
+++ ignore-build-step.sh
@@ -3,14 +3,8 @@
 # Change detection for docs/ directory
 echo "Checking for changes in docs/ directory..."
 
-# Fetch the latest changes from the remote repository
-git fetch origin
-
-# Get the latest commit hash on the main branch
-LATEST_MAIN_COMMIT=$(git rev-parse origin/main)
-
 # Check for changes in the docs/ directory since the latest commit on the main branch
-if ! git diff --quiet $LATEST_MAIN_COMMIT HEAD ./docs; then
+if ! git diff --quiet HEAD~1 HEAD ./docs; then
   echo "Changes detected in docs/, skipping build."
   exit 0
 fi
