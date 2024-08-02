d72a64b0f2cd6e5640f44082291567a925f4fcce
Gen Tamura <gen.tamura84@gmail.com>
2024-08-02 13:35:52 +0900

    Update shell script

--- ignore-build-step.sh
+++ ignore-build-step.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Change detection for docs/ directory
-if ! git diff --quiet HEAD^ HEAD ./docs; then
+if ! git diff --quiet HEAD~1 HEAD ./docs; then
   echo "Changes detected in docs/, skipping build."
   exit 0
 fi
