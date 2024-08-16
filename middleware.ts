72c16bb7c0f79f61779ee780e413e43fc08da4c8
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-08-16 15:38:23 +0900

    update

--- middleware.ts
+++ middleware.ts
@@ -3,28 +3,28 @@ import { getUserInitializationTask } from "./app/(auth)/lib";
 import { supabaseMiddleware } from "./lib/supabase";
 
 export default supabaseMiddleware(async (user, request) => {
-	// if (
-	// 	!user &&
-	// 	!request.nextUrl.pathname.startsWith("/login") &&
-	// 	!request.nextUrl.pathname.startsWith("/signup") &&
-	// 	!request.nextUrl.pathname.startsWith("/verify-email")
-	// ) {
-	// 	// no user, potentially respond by redirecting the user to the login page
-	// 	const url = request.nextUrl.clone();
-	// 	url.pathname = "/login";
-	// 	return NextResponse.redirect(url);
-	// }
-	// if (user != null) {
-	// 	const task = await getUserInitializationTask({ supabaseUserId: user.id });
-	// 	if (
-	// 		task.status !== "COMPLETED" &&
-	// 		!request.nextUrl.pathname.startsWith("/account-initialization")
-	// 	) {
-	// 		const url = request.nextUrl.clone();
-	// 		url.pathname = "/account-initialization";
-	// 		return NextResponse.redirect(url);
-	// 	}
-	// }
+	if (
+		!user &&
+		!request.nextUrl.pathname.startsWith("/login") &&
+		!request.nextUrl.pathname.startsWith("/signup") &&
+		!request.nextUrl.pathname.startsWith("/verify-email")
+	) {
+		// no user, potentially respond by redirecting the user to the login page
+		const url = request.nextUrl.clone();
+		url.pathname = "/login";
+		return NextResponse.redirect(url);
+	}
+	if (user != null) {
+		const task = await getUserInitializationTask({ supabaseUserId: user.id });
+		if (
+			task.status !== "COMPLETED" &&
+			!request.nextUrl.pathname.startsWith("/account-initialization")
+		) {
+			const url = request.nextUrl.clone();
+			url.pathname = "/account-initialization";
+			return NextResponse.redirect(url);
+		}
+	}
 });
 
 export const config = {
