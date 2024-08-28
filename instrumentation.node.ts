b1c60f74beea192f1f5466f4815b7a1fe6f34d1d
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-08-28 13:10:55 +0900

    Initialize OTEL SDK on serverside to start instrumentation

---
+++ instrumentation.node.ts
@@ -0,0 +1,30 @@
+import { metrics } from "@opentelemetry/api";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import { Resource } from "@opentelemetry/resources";
+import {
+	MeterProvider,
+	PeriodicExportingMetricReader,
+} from "@opentelemetry/sdk-metrics";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+
+// Configure OTLP metrics exporter
+const otlpMetricsExporter = new OTLPMetricExporter({
+	url: "https://ingest.us.signoz.cloud:443/v1/metrics",
+	headers: {
+		"signoz-access-token": process.env.SIGNOZ_ACCESS_TOKEN,
+	},
+});
+
+// Define resource with service name
+const sdk = new NodeSDK({
+	resource: new Resource({
+		[SEMRESATTRS_SERVICE_NAME]: "serverside",
+		environment: process.env.VERCEL_ENV || "not-set",
+	}),
+	metricReader: new PeriodicExportingMetricReader({
+		exporter: otlpMetricsExporter,
+		exportIntervalMillis: 10000,
+	}),
+});
+sdk.start();
