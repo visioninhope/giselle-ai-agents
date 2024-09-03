4b4b2be2854f2af82c07f44dbb69aeede2c23d49
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-09-03 11:28:25 +0900

    update

--- services/agents/playground/actions/get-graph.ts
+++ services/agents/playground/actions/get-graph.ts
@@ -5,30 +5,30 @@ import { eq } from "drizzle-orm";
 import { revalidateTag, unstable_cache } from "next/cache";
 import type { AgentId } from "../../types";
 
-const tag = "agent.graph";
 type GetGraphArgs = {
 	agentId: AgentId;
-	userId: string;
 };
+type TagParams = {
+	agentId: AgentId;
+};
+const tag = (params: TagParams) => `${params.agentId}.getGraph`;
 export const getGraph = async (args: GetGraphArgs) => {
-	console.log("getGraph");
 	const cached = unstable_cache(
 		async () => {
-			console.log("getGraph cache");
 			const [agent] = await db
 				.select()
 				.from(agents)
 				.where(eq(agents.id, args.agentId));
 			return agent.graph;
 		},
-		[args.userId],
+		[args.agentId],
 		{
-			tags: [tag],
+			tags: [tag(args)],
 		},
 	);
 	return await cached();
 };
 
-export const revalidateGetGraph = async () => {
-	revalidateTag(tag);
+export const revalidateGetGraph = async (params: TagParams) => {
+	revalidateTag(tag(params));
 };
--- services/agents/playground/actions/set-graph.ts
+++ services/agents/playground/actions/set-graph.ts
@@ -8,10 +8,11 @@ import type { PlaygroundGraph } from "../types";
 import { revalidateGetGraph } from "./get-graph";
 
 export const setGraph = async (agentId: AgentId, graph: PlaygroundGraph) => {
-	console.log(JSON.stringify(graph.nodes));
 	await db
 		.update(agents)
 		.set({ graph, graphHash: createId() })
 		.where(eq(agents.id, agentId));
-	await revalidateGetGraph();
+	await revalidateGetGraph({
+		agentId,
+	});
 };
