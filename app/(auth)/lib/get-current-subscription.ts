7f836f66295a5a7359d70d453e696d0103345610
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-09-05 12:57:20 +0900

    Revert "[notwork] Cache `getUserSubscriptionId()`"
    This reverts commit 18851335e672416f709bbe8d4c4eb8e6aeae6563.

--- app/(auth)/lib/get-current-subscription.ts
+++ app/(auth)/lib/get-current-subscription.ts
@@ -9,23 +9,20 @@ import {
 } from "@/drizzle";
 import { getUser } from "@/lib/supabase";
 import { eq } from "drizzle-orm";
-import { unstable_cache } from "next/cache";
 
-export const getUserSubscriptionId = unstable_cache(
-	async () => {
-		const user = await getUser();
-		const [subscription] = await db
-			.select({
-				organizationId: organizations.id, // todo: replace with 'subscriptionId' if subscriptions table is created in the future
-			})
-			.from(organizations)
-			.innerJoin(teams, eq(teams.organizationId, organizations.id))
-			.innerJoin(teamMemberships, eq(teamMemberships.teamId, teams.id))
-			.innerJoin(
-				supabaseUserMappings,
-				eq(supabaseUserMappings.userId, teamMemberships.userId),
-			)
-			.where(eq(supabaseUserMappings.supabaseUserId, user.id));
-		return subscription.organizationId;
-	}
-);
+export const getUserSubscriptionId = async () => {
+	const user = await getUser();
+	const [subscription] = await db
+		.select({
+			organizationId: organizations.id, // todo: replace with 'subscriptionId' if subscriptions table is created in the future
+		})
+		.from(organizations)
+		.innerJoin(teams, eq(teams.organizationId, organizations.id))
+		.innerJoin(teamMemberships, eq(teamMemberships.teamId, teams.id))
+		.innerJoin(
+			supabaseUserMappings,
+			eq(supabaseUserMappings.userId, teamMemberships.userId),
+		)
+		.where(eq(supabaseUserMappings.supabaseUserId, user.id));
+	return subscription.organizationId;
+};
