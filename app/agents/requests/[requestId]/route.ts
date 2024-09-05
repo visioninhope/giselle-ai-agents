58e3db6d768323eb9126f295d082f78fa5f110cd
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-09-05 11:48:22 +0900

    Count number of Agent requests

--- app/agents/requests/[requestId]/route.ts
+++ app/agents/requests/[requestId]/route.ts
@@ -1,10 +1,20 @@
 import type { RequestId } from "@/services/agents/requests";
 import { runOnVercel } from "@/services/agents/requests/runners";
+import { getUserSubscriptionId } from "@/app/(auth)/lib";
+import { metrics } from "@opentelemetry/api";
 
 export async function POST(
 	request: Request,
 	{ params }: { params: { requestId: RequestId } },
 ) {
+        const meter = metrics.getMeter("agent");
+        const requestCounter = meter.createCounter("agent_request", {
+                description: "Number of Agent requests",
+        });
+
+        requestCounter.add(1, {
+                subscriptionId: await getUserSubscriptionId(),
+        });
 	await runOnVercel({ requestId: params.requestId });
 	return new Response(null, { status: 200 });
 }
