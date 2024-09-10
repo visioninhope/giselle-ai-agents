3e7b07d84473eafc580be63d232fc89897961b16
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-09-10 14:50:16 +0900

    tweak

--- app/(auth)/password_reset/actions.ts
+++ app/(auth)/password_reset/actions.ts
@@ -8,17 +8,13 @@ export const sendPasswordResetEmail = async (
 	prevState: AuthError | null,
 	formData: FormData,
 ): Promise<AuthError | null> => {
-	const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
-	if (siteUrl == null) {
-		throw new Error("NEXT_PUBLIC_SITE_URL is not set");
-	}
 	const email = formData.get("email");
 	if (email == null || typeof email !== "string") {
 		return new AuthError("invalid_email");
 	}
 	const supabase = createClient();
 	const { error } = await supabase.auth.resetPasswordForEmail(email, {
-		redirectTo: `${siteUrl}/password_reset/new_password`,
+		redirectTo: "/password_reset/new_password",
 	});
 	if (error) {
 		return error;
