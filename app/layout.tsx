f4ebca761d75d5eb353fe54863383fd341a9c91f
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-09-11 19:00:49 +0900

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
 	title: "un-name",
@@ -21,6 +22,7 @@ export default function RootLayout({
 				>
 					{children}
 				</ThemeProvider>
+				<SpeedInsights />
 			</body>
 		</html>
 	);
