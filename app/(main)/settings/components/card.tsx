7babdf7956747f9626e8b3d6375d62eb15939b2b
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-09-11 18:24:20 +0900

    billing

--- app/(main)/settings/components/card.tsx
+++ app/(main)/settings/components/card.tsx
@@ -1,13 +1,27 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import type { FC, PropsWithChildren } from "react";
 
+type SubmitAction = {
+	href?: never;
+	content: string;
+	onAction: () => void;
+};
+type LinkAction = {
+	onAction?: never;
+	content: string;
+	href: string;
+};
+type Action = SubmitAction | LinkAction;
 type CardProps = {
 	title: string;
 	description?: string;
+	action?: Action;
 };
 export const Card: FC<PropsWithChildren<CardProps>> = ({
 	title,
 	description,
+	action,
 	children,
 }) => (
 	<div className="bg-transparent rounded-[16px] border border-black-70 py-[16px] px-[24px] w-full gap-[16px] grid">
@@ -18,7 +32,20 @@ export const Card: FC<PropsWithChildren<CardProps>> = ({
 					<p className="text-[12px] text-black-70">{description}</p>
 				)}
 			</div>
-			{/**<Button type="button">Edit</Button>**/}
+			{action && (
+				<div>
+					{action.onAction != null && (
+						<form action={action.onAction}>
+							<Button type="submit">{action.content}</Button>
+						</form>
+					)}
+					{action.href != null && (
+						<Button asChild>
+							<Link href={action.href}>{action.content}</Link>
+						</Button>
+					)}
+				</div>
+			)}
 		</div>
 		{children}
 	</div>
