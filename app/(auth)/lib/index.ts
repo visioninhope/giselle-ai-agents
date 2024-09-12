31dbe76d8536d47ac0ab1f49b0d26673da840577
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-09-12 18:20:58 +0900

    Judge user is from ROUTE06 or not to use in instrumentation

--- app/(auth)/lib/index.ts
+++ app/(auth)/lib/index.ts
@@ -1,3 +1,4 @@
 // export { getUserInitializationTask } from "./get-initialization-task";
 export { getCurrentTeam } from "./get-current-team";
 export { getUserSubscriptionId } from "./get-current-subscription";
+export { isR06User } from "./is-r06-user";
---
+++ app/(auth)/lib/is-r06-user.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { getUser } from "@/lib/supabase";
+
+const R06_EMAIL_DOMAIN = "route06.co.jp";
+
+export const isR06User = async () => {
+        const supabaseUser = await getUser();
+        const email = supabaseUser.email;
+
+        if (!email) {
+                throw new Error("No email found for user");
+        }
+
+        const emailDomain = email.split("@")[1];
+        return emailDomain === R06_EMAIL_DOMAIN;
+};
