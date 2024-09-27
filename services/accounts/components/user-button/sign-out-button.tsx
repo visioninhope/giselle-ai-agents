943395d8f978b88c21a56fcab23d53056ac0f8f5
Gen Tamura <gen.tamura84@gmail.com>
2024-09-27 14:39:31 +0900

    Add redirects to login page at logout

--- services/accounts/components/user-button/sign-out-button.tsx
+++ services/accounts/components/user-button/sign-out-button.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import type { FC } from "react";
 import { signOut } from "../../actions/sign-out";
 
 export const SignOutButton: FC = () => {
+	const router = useRouter();
+
 	return (
 		<button
 			type="button"
 			onClick={() => {
 				signOut();
+				router.push("/login");
 			}}
 		>
 			Logout
