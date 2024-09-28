eacf2a6bfa3ad3bac37771a07036901e72e3fffe
Gen Tamura <gen.tamura84@gmail.com>
2024-09-28 16:01:21 +0900

    Add actions to get member relationship by agent id

--- app/(main)/agents/[agentId]/page.tsx
+++ app/(main)/agents/[agentId]/page.tsx
@@ -1,4 +1,6 @@
 import "@xyflow/react/dist/style.css";
+
+import { getTeamMembershipByAgentId } from "@/app/(auth)/lib/get-team-membership-by-agent-id";
 import type { AgentId } from "@/services/agents";
 import { Playground, resolveOptions } from "@/services/agents/playground";
 
@@ -8,6 +10,18 @@ export default async function AgentPlaygroundPage({
 	params: { agentId: AgentId };
 }) {
 	const options = await resolveOptions();
+	const { agentId } = params;
+
+	const teamMembership = await getTeamMembershipByAgentId(agentId);
+
+	if (!teamMembership) {
+		return (
+			<div className="pt-10 flex justify-center items-center font-rosart text-3xl">
+				404 Not found
+			</div>
+		);
+	}
+
 	return (
 		<Playground
 			agentId={params.agentId}
