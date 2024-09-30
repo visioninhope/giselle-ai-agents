cf0bc0b14ff070c4d6b347793c2202806050c31d
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-09-30 15:37:59 +0900

    everyone can create account

--- app/(auth)/signup/email/signup.ts
+++ app/(auth)/signup/email/signup.ts
@@ -7,15 +7,6 @@ import { createClient } from "@/lib/supabase";
 export async function signup(email: string, password: string) {
 	const supabase = createClient();
 
-	if (!email.endsWith("@route06.co.jp")) {
-		return {
-			code: "invalid_email_domain",
-			status: 400,
-			message: "Invalid email domain.",
-			name: "InvalidEmailDomainError",
-		};
-	}
-
 	const { error } = await supabase.auth.signUp({ email, password });
 
 	if (error != null) {
