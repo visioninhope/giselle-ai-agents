85d96a11bab6f95f311d3ea30b03479fef3ae764
Koji Matsumoto <otomustam.ijok@gmail.com>
2024-10-02 14:51:44 +0900

    Revert "disable sentry"
    This reverts commit 2e6a8234218eb9978dbc8e02e4ab769b90f06f9d.

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,14 +2,14 @@ import { registerOTel } from "@vercel/otel";
 import { LangfuseExporter } from "langfuse-vercel";
 
 export async function register() {
-	// if (process.env.NEXT_RUNTIME === "nodejs") {
-	// 	await import("./sentry.server.config");
-	// 	await import("./instrumentation.node");
-	// }
+	if (process.env.NEXT_RUNTIME === "nodejs") {
+		await import("./sentry.server.config");
+		await import("./instrumentation.node");
+	}
 
-	// if (process.env.NEXT_RUNTIME === "edge") {
-	// 	await import("./sentry.edge.config");
-	// }
+	if (process.env.NEXT_RUNTIME === "edge") {
+		await import("./sentry.edge.config");
+	}
 
 	registerOTel({
 		serviceName: "giselle-dev",
