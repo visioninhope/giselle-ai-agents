78e1ef8cfbedc269b24a2c1a80b97e476457330d
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-10-09 15:50:28 +0900

    refactoring for tab ui

--- app/(playground)/p/[agentId]/beta-proto/giselle-node/components/panel/add-source-dialog.tsx
+++ app/(playground)/p/[agentId]/beta-proto/giselle-node/components/panel/add-source-dialog.tsx
@@ -60,39 +60,42 @@ export function AddSourceDialog(props: AddSourceDialogProps) {
 						This action cannot be undone. This will permanently delete your
 						account and remove your data from our servers.
 					</DialogDescription>
-					<form
-						className="p-[40px] font-rosart text-[14px] text-black-30 drop-shadow-[0px_0px_20px_0px_hsla(207,_100%,_48%,_1)] gap-[24px] flex flex-col"
-						onSubmit={handleSubmit}
-					>
+					<div className="p-[40px] font-rosart text-[14px] text-black-30 drop-shadow-[0px_0px_20px_0px_hsla(207,_100%,_48%,_1)] gap-[24px] flex flex-col">
 						<div className="border-b border-black-70 pb-[4px]">
 							Add text content
 						</div>
-						<div className="flex flex-col gap-[4px]">
-							<input
-								type="text"
-								className="bg-[hsla(0,0%,100%,0.29)] rounded-[4px] p-[12px] text-white placeholder:text-[hsla(207,43%,91%,0.6)] focus:outline focus:outline-[1px] focus:outline-black--50 font-avenir text-[12px]"
-								placeholder="Title"
-								name="title"
-								data-1p-ignore
-							/>
-							<textarea
-								placeholder="Content"
-								name="content"
-								rows={8}
-								className="bg-[hsla(0,0%,100%,0.29)] rounded-[4px] p-[12px] text-white placeholder:text-[hsla(207,43%,91%,0.6)] focus:outline focus:outline-[1px] focus:outline-black--50 font-avenir text-[12px] resize-none"
-							/>
-						</div>
-						<div className="flex items-center gap-[18px]">
-							<DialogClose asChild>
-								<Button type="button" variant="link" className="justify-center">
-									Cancel
+						<form className="gap-[24px] flex flex-col" onSubmit={handleSubmit}>
+							<div className="flex flex-col gap-[4px]">
+								<input
+									type="text"
+									className="bg-[hsla(0,0%,100%,0.29)] rounded-[4px] p-[12px] text-white placeholder:text-[hsla(207,43%,91%,0.6)] focus:outline focus:outline-[1px] focus:outline-black--50 font-avenir text-[12px]"
+									placeholder="Title"
+									name="title"
+									data-1p-ignore
+								/>
+								<textarea
+									placeholder="Content"
+									name="content"
+									rows={8}
+									className="bg-[hsla(0,0%,100%,0.29)] rounded-[4px] p-[12px] text-white placeholder:text-[hsla(207,43%,91%,0.6)] focus:outline focus:outline-[1px] focus:outline-black--50 font-avenir text-[12px] resize-none"
+								/>
+							</div>
+							<div className="flex items-center gap-[18px]">
+								<DialogClose asChild>
+									<Button
+										type="button"
+										variant="link"
+										className="justify-center"
+									>
+										Cancel
+									</Button>
+								</DialogClose>
+								<Button type="submit" className="font-[400]">
+									Complete
 								</Button>
-							</DialogClose>
-							<Button type="submit" className="font-[400]">
-								Complete
-							</Button>
-						</div>
-					</form>
+							</div>
+						</form>
+					</div>
 				</div>
 			</DialogContent>
 		</Dialog>
