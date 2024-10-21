710b4639391c45ea8dad4d3d1aced27b9ff9171f
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-10-21 16:22:36 +0900

    .gitignore app/fonts/index.ts

--- app/fonts/index.ts
+++
@@ -1,7 +0,0 @@
-import { Roboto_Serif } from "next/font/google";
-/**
- * Since the Rosart font is a proprietary font, use an alternative font during OSS development.
- */
-export const rosart = Roboto_Serif({
-	variable: "--font-rosart",
-});
