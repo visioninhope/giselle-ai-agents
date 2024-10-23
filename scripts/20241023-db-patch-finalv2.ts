13d665a134531b8cee0701444327a3d8391d92d4
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-10-23 12:22:53 +0900

    update script

---
+++ scripts/20241023-db-patch-finalv2.ts
@@ -0,0 +1,31 @@
+import {
+	type MigrateGraphV2Function,
+	migrateAgents,
+} from "./utils/agent-data-migration";
+
+// Logging the start of the operation with more detail
+console.log(
+	"Starting the update of agent data to set 'isFinal' property on nodes.",
+);
+
+// Function to update the graph of an agent
+const updateAgentGraph: MigrateGraphV2Function = (agent) => {
+	const updatedNodes = agent.graphv2.nodes.map((node) => ({
+		...node,
+		isFinal: !agent.graphv2.connectors.some(
+			(connector) => connector.source === node.id,
+		),
+	}));
+
+	return {
+		...agent.graphv2,
+		nodes: updatedNodes,
+	};
+};
+
+await migrateAgents({
+	migrateGraphV2: updateAgentGraph, // Pass the function instead of inline definition
+});
+
+// Final log statement completion
+console.log("All agents have been successfully updated!");
