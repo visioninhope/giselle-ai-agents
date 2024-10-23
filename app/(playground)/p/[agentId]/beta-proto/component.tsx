10de243f5dd99a68fec89ab501cc8ef2187757cb
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-10-23 13:15:24 +0900

    refactor

--- app/(playground)/p/[agentId]/beta-proto/component.tsx
+++ app/(playground)/p/[agentId]/beta-proto/component.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { GiselleLogo } from "@/components/giselle-logo";
 import {
 	Background,
 	BackgroundVariant,
@@ -9,11 +8,9 @@ import {
 	ReactFlowProvider,
 	useReactFlow,
 } from "@xyflow/react";
-import Link from "next/link";
 import { useState } from "react";
 import bg from "./bg.png";
 import "@xyflow/react/dist/style.css";
-import { SparklesIcon } from "./components/icons/sparkles";
 import { GradientPathDefinitions } from "./connector/gradient-definitions";
 import {
 	MousePositionProvider,
@@ -25,7 +22,6 @@ import {
 	giselleNodeArchetypes,
 	promptBlueprint,
 	textGeneratorParameterNames,
-	webSearchBlueprint,
 } from "./giselle-node/blueprints";
 import {
 	GiselleNode,
@@ -40,6 +36,7 @@ import {
 import { useGraph } from "./graph/context";
 import { GraphProvider } from "./graph/provider";
 import type { Graph } from "./graph/types";
+import { Header } from "./header";
 import {
 	type ReactFlowNode,
 	edgeTypes,
@@ -183,44 +180,7 @@ function Inner() {
 					<Toolbar />
 				</Panel>
 				<Panel position="top-left" className="!top-0 !left-0 !right-0 !m-0">
-					<div className="h-[60px] flex items-center justify-between mx-[20px]">
-						<div className="flex gap-[8px] items-center">
-							<Link href="/">
-								<GiselleLogo className="fill-white w-[70px] h-auto mt-[6px]" />
-							</Link>
-							<div className="font-rosart text-[18px] text-black--30">
-								Playground
-							</div>
-							{/**
-									<div className="flex items-center gap-[10px] group">
-										<label className="w-[30px] h-[18px] border border-black-70 rounded-full relative bg-black-80 cursor-pointer group has-[:checked]:bg-black-70 ">
-											<div className="absolute bg-black-100 rounded-full w-[16px] h-[16px] group-has-[:checked]:translate-x-[12px]  transition-all" />
-											<input type="checkbox" name="previewMode" className="hidden" />
-										</label>
-										<div className="relative font-avenir h-[18px] text-[12px]">
-											<div className="h-[18px] flex items-center absolute top-0 text-black--30 opacity-100 group-has-[:checked]:opacity-0 transition-opacity duration-400">
-												Edit
-											</div>
-											<div className="h-[18px] flex items-center  absolute text-black--30 opacity-0 group-has-[:checked]:opacity-100 transition-opacity duration-400">
-												Preview
-											</div>
-										</div>
-									</div>
-								 */}
-						</div>
-						<div>
-							<button
-								type="button"
-								className="px-[16px] py-[8px] rounded-[8px] flex items-center gap-[2px] bg-[hsla(207,19%,77%,0.3)] font-rosart"
-								style={{
-									boxShadow: "0px 0px 3px 0px hsla(0, 0%, 100%, 0.25) inset",
-								}}
-							>
-								<SparklesIcon className="w-[18px] h-[18px] fill-white drop-shadow-[0.66px_1.32px_2.64px_hsla(0,0%,100%,0.25)]" />
-								<span>Run</span>
-							</button>
-						</div>
-					</div>
+					<Header />
 				</Panel>
 				<Panel position="top-right" className="!top-0 !bottom-0 !right-0 !m-0">
 					<GiselleNodeInformationPanel />
