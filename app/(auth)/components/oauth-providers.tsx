0106f217357716316099cf2d984475bfb0ff8b8c
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-10-29 12:10:19 +0900

    Put Cotinue with GitHub button

--- app/(auth)/components/oauth-providers.tsx
+++ app/(auth)/components/oauth-providers.tsx
@@ -3,14 +3,18 @@ import { SiGithub, SiGoogle } from "@icons-pack/react-simple-icons";
 import Link from "next/link";
 import type { FC } from "react";
 
-export const OAuthProviders: FC = () => (
+type OauthProvidersProps = {
+	labelPrefix: string;
+};
+
+export const OAuthProviders: FC<OauthProvidersProps> = ({ labelPrefix }) => (
 	<div className="space-y-2">
-		<Button asChild variant="link">
+		{/* <Button asChild variant="link">
 			<Link href="/signup/google">
 				<SiGoogle className="h-[20px] w-[20px]" />
 				<p>Sign up with Google</p>
 			</Link>
-		</Button>
+		</Button> */}
 		{/**<button
 							className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
 							type="button"
@@ -21,7 +25,7 @@ export const OAuthProviders: FC = () => (
 		<Button asChild variant="link">
 			<Link href="/signup/github">
 				<SiGithub className="h-[20px] w-[20px]" />
-				<p>Sign up with GitHub</p>
+				<p>{labelPrefix} GitHub</p>
 			</Link>
 		</Button>
 	</div>
