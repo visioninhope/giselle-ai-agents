541e3f11deecc6b843a1076191a5712774d36f9d
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-10-29 14:49:47 +0900

    Initialize account if needed

--- app/(auth)/auth/callback/route.ts
+++ app/(auth)/auth/callback/route.ts
@@ -1,5 +1,8 @@
 // The client you created from the Server-Side Auth instructions
+import { db, supabaseUserMappings } from "@/drizzle";
 import { createClient } from "@/lib/supabase";
+import { initializeAccount } from "@/services/accounts";
+import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -12,7 +15,26 @@ export async function GET(request: Request) {
 		throw new Error("No code provided");
 	}
 	const supabase = await createClient();
-	const { error } = await supabase.auth.exchangeCodeForSession(code);
+	const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+
+	const user = data.user;
+	if (!user) {
+		throw new Error("No user found");
+	}
+	// initialize account if not already
+	const dbUser = await db.query.supabaseUserMappings.findFirst({
+		where: eq(supabaseUserMappings.supabaseUserId, user.id),
+	});
+	console.log(dbUser);
+	if (!dbUser) {
+		initializeAccount(user.id);
+	}
+
+	// TODO: store accessToken and refreshToken
+	if (data.session) {
+		const { provider_token, provider_refresh_token } = data.session;
+		console.log({ provider_token, provider_refresh_token });
+	}
 	if (error) {
 		const { code, message, name, status } = error;
 		throw new Error(`${name} occurred: ${code} (${status}): ${message}`);
