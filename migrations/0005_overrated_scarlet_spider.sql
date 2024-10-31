d52b20d0a41d3d0ed90b0b990defe401b3f6ebde
Koji Matsumoto <otomustam.ijok@gmail.com>
2024-10-31 18:59:20 +0900

    Revert "add migration"
    This reverts commit 09429520f97824ea8246ebf87ef516044f4bb9c1.

--- migrations/0005_overrated_scarlet_spider.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "agents" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "files" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "organizations" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "requests" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "teams" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;
