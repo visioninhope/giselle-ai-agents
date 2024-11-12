5593cda38b00c37876268fe55225beb1c6e15636
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-11-12 17:39:13 +0900

    Refactor OAuth functions

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -4,11 +4,12 @@ import { getAuthCallbackUrl } from "@/app/(auth)/lib";
 import { createClient } from "@/lib/supabase";
 import { redirect } from "next/navigation";
 
-export async function authorizeGitHub() {
-	const supabase = await createClient();
+type OAuthProvider = "github" | "google";
 
+async function authorizeOAuth(provider: OAuthProvider) {
+	const supabase = await createClient();
 	const { data, error } = await supabase.auth.signInWithOAuth({
-		provider: "github",
+		provider,
 		options: {
 			redirectTo: getAuthCallbackUrl(),
 		},
@@ -18,27 +19,16 @@ export async function authorizeGitHub() {
 		const { code, message, name, status } = error;
 		throw new Error(`${name} occurred: ${code} (${status}): ${message}`);
 	}
+
 	if (data.url) {
 		redirect(data.url);
 	}
 }
 
+export async function authorizeGitHub() {
+	return authorizeOAuth("github");
+}
+
 export async function authorizeGoogle() {
-	const supabase = await createClient();
-
-	const { data, error } = await supabase.auth.signInWithOAuth({
-		provider: "google",
-		options: {
-			redirectTo: getAuthCallbackUrl(),
-		},
-	});
-
-	if (error != null) {
-		const { code, message, name, status } = error;
-		throw new Error(`${name} occurred: ${code} (${status}): ${message}`);
-	}
-
-	if (data.url) {
-		redirect(data.url);
-	}
+	return authorizeOAuth("google");
 }
