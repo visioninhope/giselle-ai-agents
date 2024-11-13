74c9ecfada6c1d76d95dcf827130312e9368a3b6
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-11-13 09:56:03 +0900

    Add withRetry<T> utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,31 @@ export const isEmailFromRoute06 = (email: string): boolean => {
 	const domain = email.split("@")[1];
 	return domain ? domain.endsWith("route06.co.jp") : false;
 };
+
+export type withRetryOptions = {
+	retries: number;
+	onRetry?: (retryCount: number, error: unknown) => void;
+	shouldAbort?: (error: unknown) => boolean;
+};
+
+export async function withRetry<T>(
+	fn: () => Promise<T>,
+	options: withRetryOptions = {
+		retries: 10,
+	},
+): Promise<T> {
+	const { retries, onRetry = () => {}, shouldAbort = () => false } = options;
+	const errors: unknown[] = [];
+	for (let i = 0; i < retries; i++) {
+		try {
+			return await fn();
+		} catch (error: unknown) {
+			if (shouldAbort(error)) {
+				throw error;
+			}
+			errors.push(error);
+			onRetry(i, error);
+		}
+	}
+	throw new Error("Max retries reached", { cause: errors });
+}
