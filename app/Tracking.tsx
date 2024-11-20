6b1c3683cbdc92a153bf52d00fae7656a80b48bb
Koji Matsumoto <otomustam.ijok@gmail.com>
2024-11-20 18:32:16 +0900

    capture events into PostHog

---
+++ app/Tracking.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { usePostHog } from 'posthog-js/react';
+
+export function Tracking() : null {
+  if (process.env.NEXT_PUBLIC_ENABLE_ANALYTICS !== 'true') {
+    return null
+  } else {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const posthog = usePostHog();
+    useEffect(() => {
+      if (pathname && posthog) {
+        let url = window.origin + pathname
+        if (searchParams.toString()) {
+          url = url + `?${searchParams.toString()}`
+        }
+        posthog.capture(
+          '$pageview',
+          {
+            '$current_url': url,
+          }
+        )
+      }
+    }, [pathname, searchParams, posthog])
+
+    return null
+  }
+}
