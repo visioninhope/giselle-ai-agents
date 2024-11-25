22462fd92eaee4b8449942d53340cbf49f51a108
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-11-25 12:32:18 +0900

    Edit migrations to run data migration

--- migrations/0009_fuzzy_santa_claus.sql
+++ migrations/0009_fuzzy_santa_claus.sql
@@ -1,4 +1,20 @@
-ALTER TABLE "subscriptions" ADD COLUMN "team_db_id" integer NOT NULL;--> statement-breakpoint
+-- Add team_db_id column without NOT NULL constraint first
+ALTER TABLE "subscriptions" ADD COLUMN "team_db_id" integer;
+--> statement-breakpoint
+
+-- Migrate data: Set team_db_id based on organization relationship
+UPDATE subscriptions
+SET team_db_id = (
+    SELECT db_id
+    FROM teams
+    WHERE organization_db_id = subscriptions.organization_db_id
+);
+--> statement-breakpoint
+
+-- Add NOT NULL constraint after data migration
+ALTER TABLE "subscriptions" ALTER COLUMN "team_db_id" SET NOT NULL;
+--> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_team_db_id_teams_db_id_fk" FOREIGN KEY ("team_db_id") REFERENCES "public"."teams"("db_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
