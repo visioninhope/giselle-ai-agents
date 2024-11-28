3c1f486dcf4c320b5b6e127bf7a18e8caef5857b
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-11-29 08:44:19 +0900

    Rename

--- app/(auth)/lib/index.ts
+++ app/(auth)/lib/index.ts
@@ -1,9 +1,9 @@
 // export { getUserInitializationTask } from "./get-initialization-task";
 export { deleteOauthCredential } from "./delete-oauth-credential";
 export { getAuthCallbackUrl } from "./get-auth-callback-url";
-export { getUserTeamId } from "./get-current-subscription";
 export { getCurrentTeam } from "./get-current-team";
 export { getOauthCredential } from "./get-oauth-credential";
+export { getUserTeamId } from "./get-user-team-id";
 export { isRoute06User } from "./is-route06-user";
 export { refreshOauthCredential } from "./refresh-oauth-credential";
 export type { Provider } from "./types";
