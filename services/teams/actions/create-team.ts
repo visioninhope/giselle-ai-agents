3a8f3750d8cebdd96ada5f118bc5f8a4a53b9549
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-11-29 14:26:00 +0900

    Create upgradeTeam function

--- services/teams/actions/create-team.ts
+++ services/teams/actions/create-team.ts
@@ -9,7 +9,6 @@ import {
 } from "@/drizzle";
 import { getUser } from "@/lib/supabase";
 import { isEmailFromRoute06 } from "@/lib/utils";
-import { stripe } from "@/services/external/stripe";
 import type { User } from "@supabase/supabase-js";
 import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
@@ -18,6 +17,7 @@ import {
 	DRAFT_TEAM_NAME_METADATA_KEY,
 	DRAFT_TEAM_USER_DB_ID_METADATA_KEY,
 } from "../constants";
+import { createCheckoutSession } from "./create-checkout-session";
 
 export async function createTeam(formData: FormData) {
 	const teamName = formData.get("teamName") as string;
@@ -58,54 +58,19 @@ async function prepareProTeamCreation(supabaseUser: User, teamName: string) {
 async function createCheckout(userDbId: number, teamName: string) {
 	const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 	const serviceSiteUrl = process.env.NEXT_PUBLIC_SERVICE_SITE_URL;
-
 	invariant(siteUrl, "NEXT_PUBLIC_SITE_URL is not set");
 	invariant(serviceSiteUrl, "NEXT_PUBLIC_SERVICE_SITE_URL is not set");
 
-	const proPlanPriceId = process.env.STRIPE_PRO_PLAN_PRICE_ID;
-	const agentTimeChargePriceId = process.env.STRIPE_AGENT_TIME_CHARGE_PRICE_ID;
-	const userSeatPriceId = process.env.STRIPE_USER_SEAT_PRICE_ID;
-
-	invariant(proPlanPriceId, "STRIPE_PRO_PLAN_PRICE_ID is not set");
-	invariant(
-		agentTimeChargePriceId,
-		"STRIPE_AGENT_TIME_CHARGE_PRICE_ID is not set",
-	);
-	invariant(userSeatPriceId, "STRIPE_USER_SEAT_PRICE_ID is not set");
+	// FIXME: change context to the new team
+	const successUrl = `${siteUrl}/settings/team`;
+	const cancelUrl = `${serviceSiteUrl}/pricing`;
 
 	const subscriptionMetadata: Record<string, string> = {
 		[DRAFT_TEAM_USER_DB_ID_METADATA_KEY]: userDbId.toString(),
 		[DRAFT_TEAM_NAME_METADATA_KEY]: teamName,
 	};
 
-	const checkoutSession = await stripe.checkout.sessions.create({
-		mode: "subscription",
-		line_items: [
-			{
-				price: proPlanPriceId,
-				quantity: 1,
-			},
-			{
-				price: agentTimeChargePriceId,
-			},
-			{
-				price: userSeatPriceId,
-			},
-		],
-		automatic_tax: { enabled: true },
-		// FIXME: change context to the new team
-		success_url: `${siteUrl}/settings/team`,
-		cancel_url: `${serviceSiteUrl}/pricing`,
-		subscription_data: {
-			metadata: subscriptionMetadata,
-		},
-	});
-
-	if (checkoutSession.url == null) {
-		throw new Error("checkoutSession.url is null");
-	}
-
-	return checkoutSession.url;
+	return createCheckoutSession(subscriptionMetadata, successUrl, cancelUrl);
 }
 
 async function createInternalTeam(supabaseUser: User, teamName: string) {
