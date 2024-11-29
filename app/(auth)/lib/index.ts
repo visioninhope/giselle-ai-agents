f3bfbe56d13aad9f8c0bac415d682372064536e9
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-11-29 12:25:14 +0900

    Add getCurrentMesurementScope

--- app/(auth)/lib/index.ts
+++ app/(auth)/lib/index.ts
@@ -1,6 +1,7 @@
 // export { getUserInitializationTask } from "./get-initialization-task";
 export { deleteOauthCredential } from "./delete-oauth-credential";
 export { getAuthCallbackUrl } from "./get-auth-callback-url";
+export { getCurrentMeasurementScope } from "./get-current-measurement-scope";
 export { getCurrentTeam } from "./get-current-team";
 export { getOauthCredential } from "./get-oauth-credential";
 export { isRoute06User } from "./is-route06-user";
