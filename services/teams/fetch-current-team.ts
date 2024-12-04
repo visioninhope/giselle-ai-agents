602c93b50eb678aa42a8f96d503ba9f328ceb99a
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-12-04 10:12:29 +0900

    Add fallback logic

--- services/teams/fetch-current-team.ts
+++ services/teams/fetch-current-team.ts
@@ -9,28 +9,19 @@ export async function fetchCurrentTeam() {
 	const teamDbId = session?.teamDbId;
 
 	if (teamDbId == null) {
-		// if the user does not have a teamDbId in their session, return the first team
-		const team = await db
-			.select({
-				dbId: teams.dbId,
-				name: teams.name,
-			})
-			.from(teams)
-			.innerJoin(teamMemberships, eq(teams.dbId, teamMemberships.teamDbId))
-			.innerJoin(
-				supabaseUserMappings,
-				eq(teamMemberships.userDbId, supabaseUserMappings.userDbId),
-			)
-			.where(eq(supabaseUserMappings.supabaseUserId, supabaseUser.id))
-			.orderBy(asc(teams.dbId))
-			.limit(1);
-		if (team.length === 0) {
-			throw new Error("User does not have a team");
-		}
-		return team[0];
+		return fetchFirstTeam(supabaseUser.id);
 	}
 
-	const team = await db
+	const team = await fetchTeam(teamDbId, supabaseUser.id);
+	if (team == null) {
+		// fallback to first team
+		return fetchFirstTeam(supabaseUser.id);
+	}
+	return team;
+}
+
+async function fetchTeam(teamDbId: number, supabaseUserId: string) {
+	const result = await db
 		.select({
 			dbId: teams.dbId,
 			name: teams.name,
@@ -44,10 +35,31 @@ export async function fetchCurrentTeam() {
 		)
 		.where(
 			and(
-				eq(supabaseUserMappings.supabaseUserId, supabaseUser.id),
+				eq(supabaseUserMappings.supabaseUserId, supabaseUserId),
 				eq(teams.dbId, teamDbId),
 			),
 		);
+	if (result.length === 0) {
+		return null;
+	}
+	return result[0];
+}
+
+async function fetchFirstTeam(supabaseUserId: string) {
+	const team = await db
+		.select({
+			dbId: teams.dbId,
+			name: teams.name,
+		})
+		.from(teams)
+		.innerJoin(teamMemberships, eq(teams.dbId, teamMemberships.teamDbId))
+		.innerJoin(
+			supabaseUserMappings,
+			eq(teamMemberships.userDbId, supabaseUserMappings.userDbId),
+		)
+		.where(eq(supabaseUserMappings.supabaseUserId, supabaseUserId))
+		.orderBy(asc(teams.dbId))
+		.limit(1);
 
 	if (team.length === 0) {
 		throw new Error("User does not have a team");
