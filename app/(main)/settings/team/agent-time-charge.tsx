7d550462feccfac88a9c34b22e4b28a5c522b123
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-12-11 12:20:56 +0900

    Stop using decimal.js to calculate displaying number
    see https://github.com/giselles-ai/giselle/pull/200#issuecomment-2533533260

--- app/(main)/settings/team/agent-time-charge.tsx
+++ app/(main)/settings/team/agent-time-charge.tsx
@@ -1,6 +1,5 @@
 import { calculateAgentTimeUsageMs } from "@/services/agents/activities";
 import { fetchCurrentTeam, isProPlan } from "@/services/teams";
-import Decimal from "decimal.js";
 import {
 	AgentTimeUsageForFreePlan,
 	AgentTimeUsageForProPlan,
@@ -10,12 +9,8 @@ export async function AgentTimeCharge() {
 	const currentTeam = await fetchCurrentTeam();
 	const currentTeamIsPro = isProPlan(currentTeam);
 	const timeChargeMs = await calculateAgentTimeUsageMs(currentTeam.dbId);
-	const timeChargeMsDecimal = new Decimal(timeChargeMs);
 	// Round to 2 decimal places for display
-	const usedMinutes = timeChargeMsDecimal
-		.div(1000 * 60)
-		.toDecimalPlaces(2)
-		.toNumber();
+	const usedMinutes = Math.ceil((timeChargeMs / 1000 / 60) * 100) / 100;
 
 	return (
 		<div className="bg-transparent rounded-[16px] border border-black-70 py-[16px] px-[24px] w-full gap-[16px] grid">
