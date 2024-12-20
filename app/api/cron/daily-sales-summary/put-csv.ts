0078f63e2d07d902df391295c20e67e103a27922
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2024-12-20 15:30:42 +0900

    Add todo comment

--- app/api/cron/daily-sales-summary/put-csv.ts
+++ app/api/cron/daily-sales-summary/put-csv.ts
@@ -1,3 +1,4 @@
+// TODO: Upload to Cloud Storage
 export async function putCSV(path: string, header: string, content: string) {
 	console.log(`Writing to ${path}`);
 	const fileContent = header + content;
