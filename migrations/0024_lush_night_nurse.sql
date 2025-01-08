5eced42bafe6eaf7a5e33ec4d13d2bbf2c067271
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2025-01-08 09:59:31 +0900

    Populate value, is_delta for existing user_seat_usage_reports row

--- migrations/0024_lush_night_nurse.sql
+++ migrations/0024_lush_night_nurse.sql
@@ -1,2 +1,9 @@
-ALTER TABLE "user_seat_usage_reports" ADD COLUMN "value" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_seat_usage_reports" ADD COLUMN "is_delta" boolean NOT NULL;
+ALTER TABLE "user_seat_usage_reports" ADD COLUMN "value" integer;
+ALTER TABLE "user_seat_usage_reports" ADD COLUMN "is_delta" boolean;
+
+UPDATE "user_seat_usage_reports"
+SET "value" = array_length("user_db_id_list", 1),
+    "is_delta" = false;
+
+ALTER TABLE "user_seat_usage_reports" ALTER COLUMN "value" SET NOT NULL;
+ALTER TABLE "user_seat_usage_reports" ALTER COLUMN "is_delta" SET NOT NULL;
