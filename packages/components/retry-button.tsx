abee99157bd65eb5cbd344f31d88f6515b67b2e2
Gen Tamura <gen.tamura84@gmail.com>
2025-01-21 11:06:08 +0900

    Add retry button

---
+++ packages/components/retry-button.tsx
@@ -0,0 +1,34 @@
+import { Slot } from "@radix-ui/react-slot";
+import { useExecution } from "../contexts/execution";
+import type { ExecutionId, StepId } from "../types";
+
+export function RetryButton({
+	executionId,
+	stepId,
+	asChild = false,
+	children,
+}: {
+	executionId: ExecutionId;
+	stepId?: StepId;
+	asChild?: boolean;
+	children: React.ReactNode;
+}) {
+	const { retryFlowExecution } = useExecution();
+	const Comp = asChild ? Slot : "button";
+
+	const handleRetry = () => {
+		const message = stepId
+			? "Are you sure you want to retry this step?"
+			: "Are you sure you want to retry the entire flow?";
+
+		if (confirm(message)) {
+			retryFlowExecution(executionId, stepId);
+		}
+	};
+
+	return (
+		<Comp type="button" onClick={handleRetry}>
+			{children}
+		</Comp>
+	);
+}
