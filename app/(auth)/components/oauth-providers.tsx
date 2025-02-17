fdc37adf59d85983e34e9e87327b6e4458a3bb07
Akira Hayashi <akira.hayashi@route06.co.jp>
2025-02-17 10:48:44 +0900

    Remove deprecated comment-outs

--- app/(auth)/components/oauth-providers.tsx
+++ app/(auth)/components/oauth-providers.tsx
@@ -17,12 +17,6 @@ export const OAuthProviders: FC<OauthProvidersProps> = ({ labelPrefix }) => (
 				</button>
 			</form>
 		</Button>
-		{/**<button
-							className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-							type="button"
-						>
-							<Microsoft className="h-5 w-5 mr-2" /> Sign up with Microsoft
-						</button>**/}
 
 		<Button asChild variant="link">
 			<form>
