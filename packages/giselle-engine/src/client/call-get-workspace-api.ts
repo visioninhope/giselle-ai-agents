ff38248a9ff5a74aab6b9e8ceafdd8f8f03eb66b
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-02-18 18:39:57 +0900

    tweak

--- packages/giselle-engine/src/client/call-get-workspace-api.ts
+++ packages/giselle-engine/src/client/call-get-workspace-api.ts
@@ -2,15 +2,13 @@ import { Workspace, type WorkspaceId } from "@giselle-sdk/data-type";
 import { getWorkspace } from "../core/schema";
 
 export async function callGetWorkspaceApi({
-	host = process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:6180",
 	api = "/api/giselle/get-workspace",
 	workspaceId,
 }: {
 	api?: string;
-	host?: string;
 	workspaceId: WorkspaceId;
 }) {
-	const response = await fetch(`http://${host}${api}`, {
+	const response = await fetch(api, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
