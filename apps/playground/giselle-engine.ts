3185136fd4688d2f2122dc6f73ee84c50f9c7e90
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-02-20 17:32:39 +0900

    blob driver

--- apps/playground/giselle-engine.ts
+++ apps/playground/giselle-engine.ts
@@ -1,17 +1,17 @@
 import { NextGiselleEngine } from "giselle-sdk/next";
 
 import { createStorage } from "unstorage";
-import fsDriver from "unstorage/drivers/fs";
-// import vercelBlobDriver from "unstorage/drivers/vercel-blob";
+// import fsDriver from "unstorage/drivers/fs";
+import vercelBlobDriver from "unstorage/drivers/vercel-blob";
 
 const storage = createStorage({
-	driver: fsDriver({
-		base: "./.storage",
-	}),
-	// driver: vercelBlobDriver({
-	// 	access: "public",
-	// 	base: "private-beta",
+	// driver: fsDriver({
+	// 	base: "./.storage",
 	// }),
+	driver: vercelBlobDriver({
+		access: "public",
+		base: "private-beta",
+	}),
 });
 
 export const giselleEngine = NextGiselleEngine({
