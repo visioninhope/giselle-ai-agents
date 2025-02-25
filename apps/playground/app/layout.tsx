33719cf83959ef1ee9d30feb78db345b8a005304
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-02-25 20:13:54 +0900

    biome

--- apps/playground/app/layout.tsx
+++ apps/playground/app/layout.tsx
@@ -6,7 +6,7 @@ import { Hubot_Sans } from "next/font/google";
 const hubot = Hubot_Sans({
 	weight: "variable",
 	variable: "--font-hubot-sans",
-	subsets: ['latin']
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
