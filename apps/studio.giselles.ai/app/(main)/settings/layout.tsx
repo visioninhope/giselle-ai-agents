790798c61038971ec95cad453fcf991dde9e4f54
hiokky <hiokky.work@gmail.com>
2025-02-28 18:33:44 +0900

    Add conditional branching to /settings/layout.tsx

--- apps/studio.giselles.ai/app/(main)/settings/layout.tsx
+++ apps/studio.giselles.ai/app/(main)/settings/layout.tsx
@@ -1,3 +1,5 @@
+import { settingsV2Flag } from "@/flags";
+import { cn } from "@/lib/utils";
 import { UserIcon, UsersIcon } from "lucide-react";
 import type { ReactNode } from "react";
 import { IntegrationIcon } from "./components/integration-icon";
@@ -6,8 +8,10 @@ import { MenuLink } from "./components/menu-link";
 export default async function SettingLayout({
 	children,
 }: { children: ReactNode }) {
+	const settingsV2Mode = await settingsV2Flag();
+
 	return (
-		<div className="flex divide-x divide-black-80 h-full bg-black-850">
+		<div className="flex divide-x divide-black-80 h-full">
 			<div className="w-[200px] p-[24px]">
 				<div className="grid gap-[16px]">
 					<MenuLink
@@ -30,7 +34,14 @@ export default async function SettingLayout({
 					</MenuLink>
 				</div>
 			</div>
-			<div className="px-[40px] py-[24px] flex-1">{children}</div>
+			<div
+				className={cn(
+					"px-[48px] py-[32px] flex-1",
+					settingsV2Mode ? "px-[40px] py-[24px]" : "",
+				)}
+			>
+				{children}
+			</div>
 		</div>
 	);
 }
