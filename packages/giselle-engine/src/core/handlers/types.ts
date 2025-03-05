4b5379712f8216d20f32ea32b5ce2059e0546150
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-03-05 20:39:48 +0900

    remove old handler

--- packages/giselle-engine/src/core/handlers/types.ts
+++
@@ -1,5 +0,0 @@
-import type { GiselleEngineContext } from "../types";
-
-export type GiselleEngineHandlerArgs<TInput = void> = {
-	context: GiselleEngineContext;
-} & (TInput extends void ? Record<never, never> : { unsafeInput: unknown });
