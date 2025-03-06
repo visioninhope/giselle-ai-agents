f101206cbe86632280424fa1ee166f124dc9a88d
hiokky <hiokky.work@gmail.com>
2025-03-06 12:46:48 +0900

    Remove unnecessary background color and branching

--- apps/studio.giselles.ai/app/(main)/layout.tsx
+++ apps/studio.giselles.ai/app/(main)/layout.tsx
@@ -1,6 +1,4 @@
 import { GiselleLogo } from "@/components/giselle-logo";
-import { settingsV2Flag } from "@/flags";
-import { cn } from "@/lib/utils";
 import { UserButton } from "@/services/accounts/components";
 import TeamCreation from "@/services/teams/components/team-creation";
 import { TeamSelection } from "@/services/teams/components/team-selection";
@@ -9,15 +7,8 @@ import type { ReactNode } from "react";
 import { Nav } from "./nav";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-	const settingsV2Mode = await settingsV2Flag();
-
 	return (
-		<div
-			className={cn(
-				"h-screen overflow-y-hidden bg-black-100 divide-y divide-black-80 flex flex-col",
-				settingsV2Mode && "bg-black-850",
-			)}
-		>
+		<div className="h-screen overflow-y-hidden bg-black-100 divide-y divide-black-80 flex flex-col">
 			<header className="h-[60px] flex items-center px-[24px] justify-between">
 				<div className="flex">
 					<Link href="/">
