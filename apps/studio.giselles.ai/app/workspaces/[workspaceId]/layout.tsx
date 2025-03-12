886b02b7cbf0305100901db064ae1d6d2c86f445
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-03-12 18:58:33 +0900

    update app

--- apps/studio.giselles.ai/app/workspaces/[workspaceId]/layout.tsx
+++ apps/studio.giselles.ai/app/workspaces/[workspaceId]/layout.tsx
@@ -1,5 +1,8 @@
+import { db } from "@/drizzle";
+import { getGitHubIntegrationState } from "@/packages/lib/github";
 import { WorkspaceId } from "@giselle-sdk/data-type";
 import { WorkspaceProvider } from "giselle-sdk/react";
+import { notFound } from "next/navigation";
 import type { ReactNode } from "react";
 
 export default async function Layout({
@@ -10,7 +13,22 @@ export default async function Layout({
 	children: ReactNode;
 }) {
 	const workspaceId = WorkspaceId.parse((await params).workspaceId);
+
+	const agent = await db.query.agents.findFirst({
+		where: (agents, { eq }) => eq(agents.workspaceId, workspaceId),
+	});
+	if (agent === undefined) {
+		return notFound();
+	}
+	const gitHubIntegrationState = await getGitHubIntegrationState(agent.dbId);
 	return (
-		<WorkspaceProvider workspaceId={workspaceId}>{children}</WorkspaceProvider>
+		<WorkspaceProvider
+			workspaceId={workspaceId}
+			integration={{
+				github: gitHubIntegrationState,
+			}}
+		>
+			{children}
+		</WorkspaceProvider>
 	);
 }
