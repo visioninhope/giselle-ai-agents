121e44072956a98183638f0b3de34db3f0f370b2
hiokky <hiokky.work@gmail.com>
2025-03-17 16:46:58 +0900

    Add Payment Information

--- apps/studio.giselles.ai/app/(main)/settings/team/v2/billing-section.tsx
+++ apps/studio.giselles.ai/app/(main)/settings/team/v2/billing-section.tsx
@@ -13,50 +13,75 @@ export default async function BillingSection() {
 	const team = await fetchCurrentTeam();
 
 	return (
-		<Card className="flex items-center justify-between gap-x-1 py-4 border-none bg-transparent">
-			<div className="flex flex-col gap-y-1">
-				<div className="flex flex-wrap items-center gap-x-2 text-white-800 font-medium">
-					<p className="text-[14px] leading-[23.8px] font-geist">
-						Thank you for being
+		<div className="flex flex-col gap-y-4">
+			<Card className="flex items-center justify-between gap-x-1 py-4 border-none bg-transparent">
+				<div className="flex flex-col gap-y-1">
+					<div className="flex flex-wrap items-center gap-x-2 text-white-800 font-medium">
+						<p className="text-[14px] leading-[23.8px] font-geist">
+							Thank you for being
+						</p>
+						<p className="text-[22px] leading-[26.4px] tracking-[-0.04em] font-hubot">
+							{isProPlan(team) ? "Pro" : "Free"}
+						</p>
+						<p className="text-[14px] leading-[19.6px] font-hubot">
+							, {team.name}
+						</p>
+					</div>
+					<p className="text-black-400 font-medium text-[12px] leading-[20.4px] font-geist">
+						Have questions about your plan?{" "}
+						<a
+							href="https://giselles.ai/pricing"
+							target="_blank"
+							className="text-blue-80 underline"
+							rel="noreferrer"
+						>
+							Learn about plans and pricing
+						</a>
 					</p>
-					<p className="text-[22px] leading-[26.4px] tracking-[-0.04em] font-hubot">
-						{isProPlan(team) ? "Pro" : "Free"}
-					</p>
-					<p className="text-[14px] leading-[19.6px] font-hubot">
-						, {team.name}
-					</p>
+					{team.activeSubscriptionId && (
+						<Suspense fallback={<Skeleton className="h-5 w-[300px] mt-2" />}>
+							<CancellationNotice subscriptionId={team.activeSubscriptionId} />
+						</Suspense>
+					)}
 				</div>
-				<p className="text-black-400 font-medium text-[12px] leading-[20.4px] font-geist">
-					Have questions about your plan?{" "}
-					<a
-						href="https://giselles.ai/pricing"
-						target="_blank"
-						className="text-blue-80 underline"
-						rel="noreferrer"
-					>
-						Learn about plans and pricing
-					</a>
-				</p>
-				{team.activeSubscriptionId && (
-					<Suspense fallback={<Skeleton className="h-5 w-[300px] mt-2" />}>
-						<CancellationNotice subscriptionId={team.activeSubscriptionId} />
-					</Suspense>
-				)}
-			</div>
 
-			{team.type !== "internal" && (
-				<form>
-					<Suspense
-						fallback={<Skeleton className="h-10 w-[120px] rounded-md" />}
-					>
-						<BillingButton
-							subscriptionId={team.activeSubscriptionId}
-							team={team}
-						/>
-					</Suspense>
-				</form>
+				{team.type !== "internal" && (
+					<form>
+						<Suspense
+							fallback={<Skeleton className="h-10 w-[120px] rounded-md" />}
+						>
+							<BillingButton
+								subscriptionId={team.activeSubscriptionId}
+								team={team}
+							/>
+						</Suspense>
+					</form>
+				)}
+			</Card>
+
+			{isProPlan(team) && team.type !== "internal" && (
+				<Card className="flex justify-between items-center px-6 pt-4 pb-6 border-[0.5px] border-black-400 rounded-[8px] bg-transparent">
+					<div className="flex flex-col gap-y-[3px]">
+						<h2 className="text-white-400 text-[16px] leading-[27.2px] tracking-normal font-hubot">
+							Payment Information
+						</h2>
+						<p className="text-black-400 text-[12px] leading-[20.4px] tracking-normal font-geist">
+							Like by Stripe
+						</p>
+					</div>
+					<form>
+						<Suspense
+							fallback={<Skeleton className="h-10 w-[120px] rounded-md" />}
+						>
+							<BillingButton
+								subscriptionId={team.activeSubscriptionId}
+								team={team}
+							/>
+						</Suspense>
+					</form>
+				</Card>
 			)}
-		</Card>
+		</div>
 	);
 }
 
@@ -106,7 +131,7 @@ async function BillingButton({ subscriptionId, team }: BillingButtonProps) {
 	);
 	return (
 		<Button className="w-fit" formAction={manageBillingWithSubscriptionId}>
-			Manage Billing
+			Update
 		</Button>
 	);
 }
