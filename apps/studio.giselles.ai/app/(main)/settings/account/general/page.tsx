159c3541afa21eb3be7840014e747b03eb8244cc
hiokky <hiokky.work@gmail.com>
2025-03-19 19:55:08 +0900

    Add files

---
+++ apps/studio.giselles.ai/app/(main)/settings/account/general/page.tsx
@@ -0,0 +1,40 @@
+import { Field } from "@/components/v2/ui/field";
+import { settingsV2Flag } from "@/flags";
+import { notFound } from "next/navigation";
+import { Card } from "../../components/v2/card";
+import { getAccountInfo } from "../actions";
+import { AccountDisplayNameForm } from "../v2/account-display-name-form";
+
+export default async function AccountGeneralPage() {
+	const { displayName, email } = await getAccountInfo();
+	const settingsV2Mode = await settingsV2Flag();
+	if (!settingsV2Mode) {
+		return notFound();
+	}
+	return (
+		<div className="flex flex-col gap-[24px]">
+			<h3
+				className="text-primary-100 font-semibold text-[28px] leading-[28px] tracking-[-0.011em] font-hubot"
+				style={{ textShadow: "0px 0px 20px hsla(207, 100%, 48%, 1)" }}
+			>
+				General
+			</h3>
+			<div className="flex flex-col gap-y-4">
+				<AccountDisplayNameForm displayName={displayName} />
+				<Card
+					title="Email"
+					description="Enter the email addresses you want to use to log in with Vercel. Your primary email will be used for account-related notifications."
+					className="gap-y-6"
+				>
+					<Field
+						label="*Email address (required)"
+						name="email"
+						type="email"
+						value={email ?? "No email"}
+						disabled
+					/>
+				</Card>
+			</div>
+		</div>
+	);
+}
