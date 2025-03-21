5c7fe65558a041da9e12e0a42edea9abe3cd6cbc
Tadashi Shigeoka <shigeoka@codenote.net>
2025-03-21 22:36:14 +0900

    proofreading

--- apps/studio.giselles.ai/app/(main)/settings/account/general/page.tsx
+++ apps/studio.giselles.ai/app/(main)/settings/account/general/page.tsx
@@ -41,7 +41,7 @@ export default async function AccountGeneralPage() {
 					className="flex flex-row justify-between items-center gap-y-6"
 				>
 					<SignOutButton className="px-[16px] py-[4px] rounded-[6.32px] border border-primary-200 w-fit bg-primary-200 text-black-800 font-bold text-[14px] font-hubot whitespace-nowrap leading-[19.6px] tracking-normal hover:bg-transparent hover:text-primary-200">
-						Sign out
+						Log out
 					</SignOutButton>
 				</Card>
 			</div>
