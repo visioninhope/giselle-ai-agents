fac2f4c2340abb8aa59d2c2546120d30fb98d2a0
kaochannel154 <kaochannel154@gmail.com>
2025-03-26 11:20:34 +0900

    fix: update tooltip background color and remove icon titles

--- internal-packages/workflow-designer-ui/src/ui/tooltip.tsx
+++ internal-packages/workflow-designer-ui/src/ui/tooltip.tsx
@@ -27,14 +27,14 @@ export function Tooltip({
 				<TooltipPrimitive.Portal>
 					<TooltipPrimitive.Content
 						className={clsx(
-							"z-50 overflow-hidden rounded-md bg-white-850 px-3 py-1.5 text-sm text-black-850 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+							"z-50 overflow-hidden rounded-md bg-[#97A2BE] px-3 py-1.5 text-sm text-black-850 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
 							className,
 						)}
 						sideOffset={sideOffset}
 					>
 						{text}
 						<TooltipPrimitive.Arrow
-							className="fill-white-850 border-[hsla(232,36%,72%,0.2)]"
+							className="fill-[#97A2BE] border-[hsla(232,36%,72%,0.2)]"
 							width={12}
 							height={6}
 						/>
