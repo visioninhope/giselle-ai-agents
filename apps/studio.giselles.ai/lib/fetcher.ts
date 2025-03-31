ee6fe72b2832da7728d16fb96422fe9f20d8c797
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-03-31 17:21:01 +0900

    Remove unused utility files from studio.giselles.ai/lib

--- apps/studio.giselles.ai/lib/fetcher.ts
+++
@@ -1,6 +0,0 @@
-type FetchArgs = Parameters<typeof fetch>;
-export const fetcher = (...args: FetchArgs) =>
-	fetch(...args).then((res) => res.json());
-
-export const typedFetcher = <T>(...args: FetchArgs) =>
-	fetch(...args).then((res) => res.json() as T);
