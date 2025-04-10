1161e186462b8497603ac9f240ff9fb1350c8f1e
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2025-04-10 11:23:20 +0900

    fix(studio.giselles.ai): Throw error is no team is found

--- apps/studio.giselles.ai/services/teams/set-current-team.ts
+++ apps/studio.giselles.ai/services/teams/set-current-team.ts
@@ -4,6 +4,9 @@ import type { TeamId } from "./types";
 
 export async function setCurrentTeam(teamId: TeamId) {
 	const teams = await fetchUserTeams();
+	if (teams.length === 0) {
+		throw new Error("No teams found");
+	}
 	let team = teams.find((t) => t.id === teamId);
 	if (team == null) {
 		// fallback to the first team
