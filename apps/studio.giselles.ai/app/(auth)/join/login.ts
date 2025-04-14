a2ec4c0b47a1c7040b4c6eb6b0290ed09bfbcb4f
kaochannel154 <kaochannel154@gmail.com>
2025-04-14 15:18:42 +0900

    Add team invitation join page

---
+++ apps/studio.giselles.ai/app/(auth)/join/login.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { type AuthError, createClient } from "@/lib/supabase";
+import { redirect } from "next/navigation";
+
+export async function login(
+	prevState: AuthError | null,
+	formData: FormData,
+): Promise<AuthError | null> {
+	const supabase = await createClient();
+
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const credentails = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
+	const { data, error } = await supabase.auth.signInWithPassword(credentails);
+
+	if (error) {
+		return {
+			code: error.code,
+			status: error.status,
+			message: error.message,
+			name: error.name,
+		};
+	}
+	redirect("/apps");
+	return null;
+}
---
+++ apps/studio.giselles.ai/app/(auth)/join/page.tsx
@@ -0,0 +1,40 @@
+import { ClickableText } from "@/components/ui/clicable-text";
+import Link from "next/link";
+import { ActionPrompt } from "../components/action-prompt";
+import { Divider } from "../components/divider";
+import { LegalConsent } from "../components/legal-consent";
+import { OAuthProviders } from "../components/oauth-providers";
+import { LoginForm } from "./login-form";
+
+export default function Page() {
+	return (
+		<div className="min-h-screen flex items-center justify-center p-4 gap-16">
+			<div className="flex items-center justify-center py-12">
+				<div className="mx-auto grid w-[350px] gap-[24px]">
+					<h2
+						className="mt-6 text-[28px] font-[500] text-primary-100 font-hubot text-center"
+						style={{ textShadow: "0px 0px 20px #0087F6" }}
+					>
+						Log in to Giselle
+					</h2>
+					<div className="grid gap-[16px]">
+						<OAuthProviders labelPrefix="Continue" />
+						<Divider />
+						<LoginForm />
+						<LegalConsent />
+						<div className="flex justify-center">
+							<ActionPrompt
+								prompt="Don&apos;t have an account?"
+								action={
+									<ClickableText asChild>
+										<Link href="/signup">Sign up</Link>
+									</ClickableText>
+								}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
