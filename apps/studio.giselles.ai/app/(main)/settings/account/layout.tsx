ce27a95929d3c19fe636283c071218452b97daad
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2025-04-15 10:50:14 +0900

    fix(studio.giselles.ai): Fix style for current displaying menu

--- apps/studio.giselles.ai/app/(main)/settings/account/layout.tsx
+++ apps/studio.giselles.ai/app/(main)/settings/account/layout.tsx
@@ -1,6 +1,5 @@
-import { ExternalLink } from "lucide-react";
-import Link from "next/link";
 import type { ReactNode } from "react";
+import { SidebarMenu } from "./sidebar-menu";
 
 export default async function SettingsAccountLayout({
 	children,
@@ -9,30 +8,7 @@ export default async function SettingsAccountLayout({
 		<div className="h-full bg-black-900">
 			<div className="px-[40px] pb-[24px] flex-1 max-w-[1200px] mx-auto w-full flex divide-x divide-black-80">
 				{/* Left Sidebar */}
-				<div className="w-[240px] min-h-full flex flex-col pt-[24px]">
-					{/* Menu Items */}
-					<div className="flex flex-col space-y-4">
-						<Link
-							href="/settings/account"
-							className="text-white-400 hover:text-white-100 text-[16px] font-hubot font-medium py-1"
-						>
-							Overview
-						</Link>
-						<Link
-							href="/settings/account/general"
-							className="text-black-70 hover:text-white-100 text-[16px] font-hubot font-medium py-1"
-						>
-							General
-						</Link>
-						<Link
-							href="/settings/account/authentication"
-							className="text-black-70 hover:text-white-100 text-[16px] font-hubot font-medium py-1"
-						>
-							Authentication
-						</Link>
-					</div>
-				</div>
-
+				<SidebarMenu />
 				{/* Main Content */}
 				<div className="pl-[24px] flex-1 pt-[24px]">{children}</div>
 			</div>
