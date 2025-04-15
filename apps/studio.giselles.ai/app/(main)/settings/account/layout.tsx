e99361089e1e0b48a928c034b27c830cc4268d67
kaochannel154 <kaochannel154@gmail.com>
2025-04-15 13:52:23 +0900

    Fix sidebar border to extend to the bottom without padding

--- apps/studio.giselles.ai/app/(main)/settings/account/layout.tsx
+++ apps/studio.giselles.ai/app/(main)/settings/account/layout.tsx
@@ -6,9 +6,11 @@ export default async function SettingsAccountLayout({
 }: { children: ReactNode }) {
 	return (
 		<div className="h-full bg-black-900">
-			<div className="px-[40px] pb-[24px] flex-1 max-w-[1200px] mx-auto w-full flex divide-x divide-black-80">
-				{/* Left Sidebar */}
-				<SidebarMenu />
+			<div className="px-[40px] flex-1 max-w-[1200px] mx-auto w-full flex min-h-[calc(100vh-64px)]">
+				{/* Left Sidebar with border */}
+				<div className="border-r border-black-80">
+					<SidebarMenu />
+				</div>
 				{/* Main Content */}
 				<div className="pl-[24px] flex-1 pt-[24px]">{children}</div>
 			</div>
