b8815dc271ab4cbd30e66cad82a3670261e7f5e8
Yuta Hiroto <git@hiroppy.me>
2025-04-27 11:30:34 +0900

    feat(studio): add some security headers

--- apps/studio.giselles.ai/next.config.ts
+++ apps/studio.giselles.ai/next.config.ts
@@ -51,6 +51,22 @@ const nextConfig: NextConfig = {
 						key: "X-Frame-Options",
 						value: "DENY",
 					},
+					{
+						key: "X-DNS-Prefetch-Control",
+						value: "on",
+					},
+					{
+						key: "X-XSS-Protection",
+						value: "1; mode=block",
+					},
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "origin-when-cross-origin",
+					},
 				],
 			},
 		];
