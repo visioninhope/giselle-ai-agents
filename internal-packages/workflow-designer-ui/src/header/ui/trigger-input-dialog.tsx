f9daed1329988ffa6994204418963737ab66ed9a
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-05-15 15:04:23 +0900

    Update trigger-input-dialog.tsx

--- internal-packages/workflow-designer-ui/src/header/ui/trigger-input-dialog.tsx
+++ internal-packages/workflow-designer-ui/src/header/ui/trigger-input-dialog.tsx
@@ -12,6 +12,7 @@ import { PlayIcon, XIcon } from "lucide-react";
 import { Dialog } from "radix-ui";
 import {
 	type ButtonHTMLAttributes,
+	type FormEventHandler,
 	type ReactNode,
 	useCallback,
 	useMemo,
@@ -54,63 +55,21 @@ export function buttonLabel(triggerProvider: TriggerProvider) {
 }
 
 export function TriggerButton({ triggerNode }: { triggerNode: TriggerNode }) {
-	const { data: workspace } = useWorkflowDesigner();
-	const { createGeneration, startGeneration } = useGenerationRunnerSystem();
 	const { data, isLoading } = useTrigger(triggerNode);
 
-	const handleClick = useCallback(async () => {
-		const flow = buildWorkflowFromNode(
-			triggerNode.id,
-			workspace.nodes,
-			workspace.connections,
-		);
-		if (flow === null) {
-			return;
-		}
-		const generations: Generation[] = [];
-		flow.jobs.map((job) =>
-			job.operations.map((operation) => {
-				generations.push(
-					createGeneration({
-						origin: {
-							type: "workspace",
-							id: workspace.id,
-						},
-						...operation.generationTemplate,
-					}),
-				);
-			}),
-		);
-		for (const job of flow.jobs) {
-			for (const operation of job.operations) {
-				const generation = generations.find(
-					(generation) =>
-						generation.context.operationNode.id ===
-						operation.generationTemplate.operationNode.id,
-				);
-				if (generation === undefined) {
-					continue;
-				}
-				await startGeneration(generation.id);
-			}
-		}
-	}, [triggerNode.id, workspace, createGeneration, startGeneration]);
 	if (isLoading || data === undefined) {
 		return null;
 	}
 	return (
 		<Dialog.Root>
 			<Dialog.Trigger asChild>
-				<Button
-					leftIcon={<PlayIcon className="size-[14px] fill-black-900" />}
-					onClick={handleClick}
-				>
+				<Button leftIcon={<PlayIcon className="size-[14px] fill-black-900" />}>
 					{buttonLabel(triggerNode.content.provider)}
 				</Button>
 			</Dialog.Trigger>
 			<Dialog.Portal>
 				<Dialog.Overlay className="fixed inset-0 bg-black/25 z-50" />
-				<Dialog.Content className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[900px] h-[600px] bg-black-900 rounded-[12px] p-[24px] shadow-xl z-50 overflow-hidden border border-black-400 outline-none">
+				<Dialog.Content className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[400px] bg-black-900 rounded-[12px] p-[24px] shadow-xl z-50 overflow-hidden border border-black-400 outline-none">
 					<Dialog.Title className="sr-only">
 						Override inputs to test workflow
 					</Dialog.Title>
@@ -150,6 +109,8 @@ export function TriggerInputDialog({
 	node: TriggerNode;
 	trigger: FlowTrigger;
 }) {
+	const { createGeneration, startGeneration } = useGenerationRunnerSystem();
+	const { data } = useWorkflowDesigner();
 	const inputs = useMemo<Input[]>(() => {
 		switch (trigger.configuration.provider) {
 			case "github": {
@@ -177,7 +138,6 @@ export function TriggerInputDialog({
 							},
 						];
 					case "github.issue_comment.created":
-						githubTrigger.event.payloads;
 						return [
 							{
 								name: "issueId",
@@ -224,21 +184,59 @@ export function TriggerInputDialog({
 			}
 		}
 	}, [trigger]);
+
+	const handleSubmit = useCallback<FormEventHandler<HTMLFormElement>>(
+		async (e) => {
+			e.preventDefault();
+			const formData = new FormData(e.currentTarget);
+
+			const flow = buildWorkflowFromNode(node.id, data.nodes, data.connections);
+			if (flow === null) {
+				return;
+			}
+			const generations: Generation[] = [];
+			flow.jobs.map((job) =>
+				job.operations.map((operation) => {
+					generations.push(
+						createGeneration({
+							origin: {
+								type: "workspace",
+								id: data.id,
+							},
+							...operation.generationTemplate,
+						}),
+					);
+				}),
+			);
+			for (const job of flow.jobs) {
+				for (const operation of job.operations) {
+					const generation = generations.find(
+						(generation) =>
+							generation.context.operationNode.id ===
+							operation.generationTemplate.operationNode.id,
+					);
+					if (generation === undefined) {
+						continue;
+					}
+					await startGeneration(generation.id);
+				}
+			}
+		},
+		[node.id, data, createGeneration, startGeneration],
+	);
 	return (
 		<div className="flex flex-col h-full">
-			<form className="flex-1 flex flex-col gap-[24px] relative text-white-800 overflow-y-hidden">
+			<form
+				className="flex-1 flex flex-col gap-[24px] relative text-white-800 overflow-y-hidden"
+				onSubmit={handleSubmit}
+			>
 				<p className="text-[12px] mb-[8px] text-black-400 font-hubot font-semibold">
 					Execute this flow with custom input values
 				</p>
 				<div className="flex flex-col gap-[8px]">
 					{inputs.map((input) => {
 						return (
-							<fieldset
-								key={input.name}
-								className={clsx("grid gap-2")}
-								// onClick={() => handleNodeSelect(output.id)}
-								// aria-pressed={activeNodeId === output.id}
-							>
+							<fieldset key={input.name} className={clsx("grid gap-2")}>
 								<label
 									className="text-[14px] font-medium text-white-900"
 									htmlFor={input.name}
@@ -274,7 +272,12 @@ export function TriggerInputDialog({
 					})}
 				</div>
 				<div className="flex justify-end">
-					<Button type="submit">Run with params</Button>
+					<Button
+						type="submit"
+						leftIcon={<PlayIcon className="size-[14px] fill-black-900" />}
+					>
+						Run with params
+					</Button>
 				</div>
 			</form>
 		</div>
