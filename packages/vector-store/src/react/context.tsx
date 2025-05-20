ee9a4b75c5a3e5892790c9ead21204d8333a18e7
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2025-05-20 14:11:36 +0900

    feat(vector-store): Extract vector-store package

---
+++ packages/vector-store/src/react/context.tsx
@@ -0,0 +1,34 @@
+import { type PropsWithChildren, createContext, useContext } from "react";
+import type { GitHubVectorStoreInfo } from "../types";
+
+export interface VectorStoreContextValue {
+	github: GitHubVectorStoreInfo[];
+}
+
+export const VectorStoreContext = createContext<
+	Partial<VectorStoreContextValue> | undefined
+>(undefined);
+
+export interface VectorStoreProviderProps {
+	value?: Partial<VectorStoreContextValue>;
+}
+export function VectorStoreProvider({
+	children,
+	...props
+}: PropsWithChildren<VectorStoreProviderProps>) {
+	return (
+		<VectorStoreContext.Provider value={props.value}>
+			{children}
+		</VectorStoreContext.Provider>
+	);
+}
+
+export const useVectorStore = () => {
+	const vectorStore = useContext(VectorStoreContext);
+	if (!vectorStore) {
+		throw new Error(
+			"useVectorStore must be used within an VectorStoreProvider",
+		);
+	}
+	return vectorStore;
+};
--- packages/vector-store/src/types.ts
+++ packages/vector-store/src/types.ts
@@ -4,14 +4,6 @@ type VectorStoreReference<P extends Provider, I extends object> = I & {
 	provider: P;
 };
 
-export type GitHubVectorStoreReference = VectorStoreReference<
-	"github",
-	{
-		owner: string;
-		repo: string;
-	}
->;
-
 /*
   If we need another provider, we can add it here.
 
@@ -44,9 +36,15 @@ export type VectorStoreIdentifier<
 	T extends VectorStoreReference<Provider, object>,
 > = Omit<T, "provider">;
 
-export type VectorStoreAdaptor<
-	T extends VectorStoreReference<Provider, object>,
-> = {
-	getVectorStoreList: () => VectorStoreInfo<T>[];
-	getVectorStoreReference: (identifier: VectorStoreIdentifier<T>) => T;
-};
+// MARK: GitHubVectorStore
+
+type GitHubVectorStoreReference = VectorStoreReference<
+	"github",
+	{
+		owner: string;
+		repo: string;
+	}
+>;
+export type GitHubVectorStoreInfo = VectorStoreInfo<GitHubVectorStoreReference>;
+export type GitHubVectorStoreIdentifier =
+	VectorStoreIdentifier<GitHubVectorStoreReference>;
