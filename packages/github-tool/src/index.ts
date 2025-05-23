f5ff1be9e4b20eb13cf15a8ef2fd2c761806686b
toyamarinyon <satoshi.toyama@route06.co.jp>
2025-05-23 16:08:31 +0900

    Update packages/github-tool/src/index.ts
    Co-authored-by: Tadashi Shigeoka <tadashi.shigeoka@route06.co.jp>

--- packages/github-tool/src/index.ts
+++ packages/github-tool/src/index.ts
@@ -1,7 +1,6 @@
 import { graphql as gql } from "./graphql";
 export * from "./blob-loader";
 export * from "./issues";
-export { createPullRequestComment } from "./issues";
 export * from "./octokit";
 export * from "./repository";
 export * from "./tools";
