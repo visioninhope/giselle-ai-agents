1c8e7d1539856f60f7cc13160f8ab222bba1b913
Tadashi Shigeoka <tadashi.shigeoka@route06.co.jp>
2025-05-26 21:03:45 +0900

    Add webPage to packages/giselle-engine

--- packages/giselle-engine/src/core/generations/utils.ts
+++ packages/giselle-engine/src/core/generations/utils.ts
@@ -163,7 +163,8 @@ async function buildGenerationMessageForTextGeneration(
 						break;
 					}
 					case "image":
-					case "pdf": {
+					case "pdf":
+					case "webPage": {
 						const fileContents = await getFileContents(
 							contextNode.content,
 							fileResolver,
@@ -477,6 +478,7 @@ async function getFileContents(
 			switch (fileContent.category) {
 				case "pdf":
 				case "text":
+				case "webPage":
 					return {
 						type: "file",
 						data,
@@ -589,7 +591,8 @@ async function buildGenerationMessageForImageGeneration(
 				switch (contextNode.content.category) {
 					case "text":
 					case "image":
-					case "pdf": {
+					case "pdf":
+					case "webPage": {
 						const fileContents = await getFileContents(
 							contextNode.content,
 							fileResolver,
