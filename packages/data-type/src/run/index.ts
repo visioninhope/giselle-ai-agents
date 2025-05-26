378546cd9ada368cde2937302048d0cc2950d7a2
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-05-26 19:08:46 +0900

    zod4 for high layer

--- packages/data-type/src/run/index.ts
+++ packages/data-type/src/run/index.ts
@@ -1,5 +1,5 @@
-import { createIdGenerator } from "@giselle-sdk/utils";
-import { z } from "zod";
+import { createIdGeneratorV4 as createIdGenerator } from "@giselle-sdk/utils";
+import { z } from "zod/v4";
 import { OverrideNode } from "../node";
 import { Workflow } from "../workflow";
 import { WorkspaceId } from "../workspace";
