25c846684eae8501689f48c711158abcc86d62da
toyamarinyon <toyamarinyon@gmail.com>
2025-06-02 16:42:44 +0900

    feat(github-tool): add getPullRequestReviewComment

--- packages/github-tool/src/pull-requests.ts
+++ packages/github-tool/src/pull-requests.ts
@@ -31,3 +31,30 @@ export async function getPullRequestDiff(args: {
 	);
 	return response.data as unknown as string;
 }
+
+export async function getPullRequestReviewComment(args: {
+	repositoryNodeId: string;
+	commentId: number;
+	authConfig: GitHubAuthConfig;
+}) {
+	const client = octokit(args.authConfig);
+	const repo = await getRepositoryFullname(
+		args.repositoryNodeId,
+		args.authConfig,
+	);
+	if (repo.error || repo.data === undefined) {
+		throw new Error(`Failed to get repository information: ${repo.error}`);
+	}
+	if (repo.data.node?.__typename !== "Repository") {
+		throw new Error(`Invalid repository type: ${repo.data.node?.__typename}`);
+	}
+	const response = await client.request(
+		"GET /repos/{owner}/{repo}/pulls/comments/{comment_id}",
+		{
+			owner: repo.data.node.owner.login,
+			repo: repo.data.node.name,
+			comment_id: args.commentId,
+		},
+	);
+	return response.data;
+}
