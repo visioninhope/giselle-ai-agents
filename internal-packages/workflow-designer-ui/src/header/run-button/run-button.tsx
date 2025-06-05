31878b18b507abbeae309b6fbadac8a230211154
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-06-05 13:38:17 +0900

    Add dialog support for running trigger nodes

--- internal-packages/workflow-designer-ui/src/header/run-button/run-button.tsx
+++ internal-packages/workflow-designer-ui/src/header/run-button/run-button.tsx
@@ -2,13 +2,17 @@ import { isOperationNode, isTriggerNode } from "@giselle-sdk/data-type";
 import { defaultName } from "@giselle-sdk/node-utils";
 import clsx from "clsx/lite";
 import { useWorkflowDesigner } from "giselle-sdk/react";
-import { DropdownMenu } from "radix-ui";
-import { useMemo } from "react";
+import { Dialog, DropdownMenu } from "radix-ui";
+import { useMemo, useState } from "react";
 import { NodeIcon } from "../../icons/node";
+import { TriggerInputDialog } from "../ui";
 import { Button } from "./ui/button";
 
 export function RunButton() {
 	const { data } = useWorkflowDesigner();
+
+	const [isDialogOpen, setIsDialogOpen] = useState(false);
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 	const startingNodes = useMemo(() => {
 		const triggerNodes = data.nodes.filter((node) => isTriggerNode(node));
 		const startingOperationNodes = data.nodes.filter((node) => {
@@ -26,7 +30,7 @@ export function RunButton() {
 		return [...triggerNodes, ...startingOperationNodes];
 	}, [data.nodes]);
 	return (
-		<DropdownMenu.Root>
+		<DropdownMenu.Root open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
 			<DropdownMenu.Trigger asChild>
 				<Button>Run</Button>
 			</DropdownMenu.Trigger>
@@ -42,19 +46,70 @@ export function RunButton() {
 				>
 					<div className="absolute z-0 rounded-[8px] inset-0 border mask-fill bg-gradient-to-br from-[hsla(232,37%,72%,0.2)] to-[hsla(218,58%,21%,0.9)] bg-origin-border bg-clip-border border-transparent" />
 					{startingNodes.map((startingNode) => (
-						<DropdownMenu.Item
-							className="group relative flex items-center py-[8px] px-[12px] gap-[6px] outline-none cursor-pointer"
-							key={startingNode.id}
-						>
-							<NodeIcon node={startingNode} className="size-[24px]" />
-							<div className="flex flex-col gap-[0px] text-white-900">
-								<div className="text-[13px]">
-									{startingNode.name ?? defaultName(startingNode)}
-								</div>
-								<div className="text-[12px] text-white-800">
-									{startingNode.id}
-								</div>
-							</div>
+						<DropdownMenu.Item key={startingNode.id} asChild>
+							{isTriggerNode(startingNode) ? (
+								<Dialog.Root
+									open={isDialogOpen}
+									onOpenChange={(isOpen) => {
+										setIsDialogOpen(isOpen);
+									}}
+								>
+									<Dialog.Trigger className="group relative flex items-center py-[8px] px-[12px] gap-[10px] outline-none cursor-pointer hover:bg-black-400/20 rounded-[6px] w-full">
+										<>
+											<div className="p-[12px] bg-black-800 rounded-[8px]">
+												<NodeIcon
+													node={startingNode}
+													className="size-[16px] text-white-900"
+												/>
+											</div>
+											<div className="flex flex-col gap-[0px] text-white-900 items-start">
+												<div className="text-[13px]">
+													{startingNode.name ?? defaultName(startingNode)}
+												</div>
+												<div className="text-[12px] text-white-400">
+													{startingNode.id}
+												</div>
+											</div>
+										</>
+									</Dialog.Trigger>
+									<Dialog.Portal>
+										<Dialog.Overlay className="fixed inset-0 bg-black/25 z-50" />
+										<Dialog.Content className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[400px] bg-black-900 rounded-[12px] p-[24px] shadow-xl z-50 overflow-hidden border border-black-400 outline-none">
+											<Dialog.Title className="sr-only">
+												Override inputs to test workflow
+											</Dialog.Title>
+
+											<TriggerInputDialog
+												node={startingNode}
+												onClose={() => {
+													setIsDropdownOpen(false);
+													setIsDialogOpen(false);
+												}}
+											/>
+										</Dialog.Content>
+									</Dialog.Portal>
+								</Dialog.Root>
+							) : (
+								<button
+									type="button"
+									className="group relative flex items-center py-[8px] px-[12px] gap-[10px] outline-none cursor-pointer hover:bg-black-400/20 rounded-[6px] w-full"
+								>
+									<div className="p-[12px] bg-black-800 rounded-[8px]">
+										<NodeIcon
+											node={startingNode}
+											className="size-[16px] text-white-900"
+										/>
+									</div>
+									<div className="flex flex-col gap-[0px] text-white-900 items-start">
+										<div className="text-[13px]">
+											{startingNode.name ?? defaultName(startingNode)}
+										</div>
+										<div className="text-[12px] text-white-400">
+											{startingNode.id}
+										</div>
+									</div>
+								</button>
+							)}
 						</DropdownMenu.Item>
 					))}
 				</DropdownMenu.Content>
--- internal-packages/workflow-designer-ui/src/header/run-button/ui/button.tsx
+++ internal-packages/workflow-designer-ui/src/header/run-button/ui/button.tsx
@@ -21,7 +21,7 @@ export function Button({
 		<button
 			type="button"
 			className={clsx(
-				"bg-white-900 px-[12px] rounded-[4px] py-[4px] text-[14px] flex items-center gap-[4px] outline-none text-black-900 font-medium font-accent cursor-pointer",
+				"bg-black-750 px-[14px] rounded-[4px] py-[4px] text-[14px] flex items-center gap-[4px] outline-none text-black-200 font-medium font-accent cursor-pointer hover:bg-black-800 data-[state=open]:bg-black-800",
 				"data-[loading=true]:cursor-not-allowed data-[loading=true]:opacity-60",
 				"data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-60",
 				"data-[loading=false]:data-[disabled=false]:cursor-pointer",
