37ac4103b53f86880b094fe86bde6abbcdc59b16
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-06-10 14:12:25 +0900

    Remove client-side page split and simplify workspace page

--- apps/studio.giselles.ai/app/workspaces/[workspaceId]/page.tsx
+++ apps/studio.giselles.ai/app/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,13 @@
-import Page from "./page.client";
+"use client";
 
-export default function () {
-	return <Page />;
+import { Editor, Header } from "@giselle-internal/workflow-designer-ui";
+import { updateAgentName } from "./actions";
+
+export default function Page() {
+	return (
+		<div className="flex flex-col h-screen bg-black-900">
+			<Header onWorkflowNameChange={updateAgentName} />
+			<Editor />
+		</div>
+	);
 }
