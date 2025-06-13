1b691ac497f2a9c41196e355ab932bb2ec2d2884
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-06-13 14:30:29 +0900

    Update dropdown menu shadow styling

--- internal-packages/workflow-designer-ui/src/editor/properties-panel/text-generation-node-properties-panel/tools/ui/dropdown-menu.tsx
+++ internal-packages/workflow-designer-ui/src/editor/properties-panel/text-generation-node-properties-panel/tools/ui/dropdown-menu.tsx
@@ -32,7 +32,8 @@ export function DropdownMenu<T extends Identifiable>({
 					sideOffset={4}
 					className={clsx(
 						"rounded-[2px] w-(--radix-dropdown-menu-trigger-width) bg-panel-background z-50",
-						"p-[4px] border border-border-variant shadow-md",
+						"p-[4px] border border-border-variant",
+						"shadow-[1px_1px_0px_rgba(0,0,0,0.05),-1px_1px_0px_rgba(0,0,0,0.05),0_2px_6px_rgba(0,0,0,0.30)]",
 					)}
 				>
 					{items.map((item) => (
