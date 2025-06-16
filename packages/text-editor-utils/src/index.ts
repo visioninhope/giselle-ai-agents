7be90f71aacf180fc609f8f693289c3cce04a3d2
Gen Tamura <gen.tamura84@gmail.com>
2025-06-16 10:28:31 +0900

    refactor: Move node-reference-cleanup.ts to text-editor-utils
    Move node-reference-cleanup functions to text-editor-utils package where
    they belong alongside other node reference utilities (node-reference.ts).
    This creates better cohesion for all node reference operations in one place.

--- packages/text-editor-utils/src/index.ts
+++ packages/text-editor-utils/src/index.ts
@@ -2,4 +2,5 @@ export * from "./extensions";
 export * from "./is-json-content";
 export * from "./json-content-to-text";
 export * from "./node-reference";
+export * from "./node-reference-cleanup";
 export * from "./detect-source-changes";
