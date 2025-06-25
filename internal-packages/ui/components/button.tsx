dea418b471c06c889b124e4e2edd107c4a0b1290
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-06-25 17:01:29 +0900

    Enhance Button component with variants and size options

--- internal-packages/ui/components/button.tsx
+++ internal-packages/ui/components/button.tsx
@@ -1,11 +1,13 @@
 import clsx from "clsx/lite";
 
 type ButtonStyle = "subtle" | "filled" | "solid" | "glassmorphic";
+type ButtonSize = "compact" | "default" | "large";
 interface ButtonProps
 	extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "style"> {
 	leftIcon?: React.ReactNode;
 	rightIcon?: React.ReactNode;
 	variant?: ButtonStyle;
+	size?: ButtonSize;
 }
 
 export function Button({
@@ -14,12 +16,15 @@ export function Button({
 	leftIcon,
 	rightIcon,
 	variant: style = "subtle",
+	size = "default",
 	...props
 }: ButtonProps) {
 	return (
 		<button
 			className={clsx(
-				"flex items-center justify-between gap-[4px] px-[8px] py-[2px] rounded-[2px] outline-none overflow-hidden",
+				"flex items-center justify-between gap-[4px] outline-none overflow-hidden",
+				"data-[size=default]:px-[8px] data-[size=default]:py-[2px] data-[size=default]:rounded-[2px] ",
+				"data-[size=large]:px-4 data-[size=large]:py-2 data-[size=large]:rounded-lg",
 				"data-[style=subtle]:hover:bg-ghost-element-hover",
 				"data-[style=filled]:bg-background data-[style=filled]:border data-[style=filled]:border-border data-[style=filled]:hover:bg-ghost-element-hover",
 				"data-[style=solid]:bg-primary-900 data-[style=solid]:text-white-900 data-[style=solid]:border data-[style=solid]:border-primary-800 data-[style=solid]:hover:bg-primary-800 data-[style=sold]:hover:bg-ghost-element-hover",
@@ -30,22 +35,9 @@ export function Button({
 				className,
 			)}
 			data-style={style}
+			data-size={size}
 			{...props}
 		>
-			{style === "glassmorphic" && (
-				<>
-					{/* <div data-glassmorphic-button-background className=" absolute inset-0 rounded-[2px] backdrop-blur-md"/> */}
-					{/* <div className="bg-linear-[135deg] from-glassmorphic-bg-gradient-from via-glassmorphic-bg-gradient-via to-glassmorphic-bg-gradient-to absolute inset-0 rounded-[2px] backdrop-blur-md" /> */}
-					{/* Top reflection */}
-					<div className="bg-linear-to-r from-transparent via-glassmorphic-highlight/60 to-transparent absolute top-0 left-4 right-4 h-px" />
-
-					{/* Subtle border */}
-					<div className="absolute inset-0 rounded-[2px] border border-glassmorphic-border/20" />
-
-					{/* Hover overlay */}
-					{/* <div className="absolute inset-0 rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100 bg-top from-transparent to-glassmorphic-highlight/10" /> */}
-				</>
-			)}
 			{leftIcon && <div className="*:size-[13px] *:text-text">{leftIcon}</div>}
 			<div className="text-[13px] text-text">{children}</div>
 			{rightIcon && <div className="*:size-[13px]">{rightIcon}</div>}
