e66628c4078c307e68492c2680f23b63a1fc8124
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2025-07-01 15:13:20 +0900

    Revert "chore(studio): add migration"
    This reverts commit 684b52b160a3bb42666c62de60e06459d8813f0b.

--- apps/studio.giselles.ai/migrations/0035_sad_quasar.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "github_repository_index" ADD COLUMN "error_code" text;--> statement-breakpoint
-ALTER TABLE "github_repository_index" ADD COLUMN "is_retryable" boolean;--> statement-breakpoint
-ALTER TABLE "github_repository_index" ADD COLUMN "retry_after" timestamp;
