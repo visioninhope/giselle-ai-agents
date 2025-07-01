32355fa857329403c49ab9ef52a1524423eb9928
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-07-01 10:36:59 +0900

    Fix import path for defaultName from giselle-engine/react

--- packages/text-editor/src/react/source-extension-react.tsx
+++ packages/text-editor/src/react/source-extension-react.tsx
@@ -1,5 +1,5 @@
 import { Node as GiselleNode } from "@giselle-sdk/data-type";
-import { defaultName } from "@giselle-sdk/giselle-engine";
+import { defaultName } from "@giselle-sdk/giselle-engine/react";
 import { SourceExtension } from "@giselle-sdk/text-editor-utils";
 import {
 	type NodeViewProps,
