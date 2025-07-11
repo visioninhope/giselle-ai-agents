a09b624dae87303b0f116c348eecf04436f0501b
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-07-11 17:04:27 +0900

    Add FlowTriggerContext and provider to workspace

---
+++ packages/giselle-engine/src/react/flow-trigger/context.ts
@@ -0,0 +1,21 @@
+import type { FlowTrigger } from "@giselle-sdk/data-type";
+import { createContext, useContext } from "react";
+
+type FlowTriggerUpdateCallback = (flowTrigger: FlowTrigger) => Promise<void>;
+export interface FlowTriggerContextValue {
+	callbacks?: {
+		flowTriggerUpdate?: FlowTriggerUpdateCallback;
+	};
+}
+
+export const FlowTriggerContext = createContext<
+	FlowTriggerContextValue | undefined
+>(undefined);
+
+export function useFlowTrigger() {
+	const context = useContext(FlowTriggerContext);
+	if (!context) {
+		throw new Error("useFlowTrigger must be used within a FlowTriggerProvider");
+	}
+	return context;
+}
