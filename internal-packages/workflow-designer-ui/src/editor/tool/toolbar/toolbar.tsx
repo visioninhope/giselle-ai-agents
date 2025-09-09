f7a1467ccdcec5d28943855bd9aee4b84a25d9ad
Kaori Nakashima <kaochannel154@gmail.com>
2025-09-09 09:37:55 +0900

    Update internal-packages/workflow-designer-ui/src/editor/tool/toolbar/toolbar.tsx
    I think this was an oversight on my end. My apologies, and thank you for pointing it out!
    
    Co-authored-by: Tadashi Shigeoka <tadashi.shigeoka@route06.co.jp>

--- internal-packages/workflow-designer-ui/src/editor/tool/toolbar/toolbar.tsx
+++ internal-packages/workflow-designer-ui/src/editor/tool/toolbar/toolbar.tsx
@@ -547,7 +547,7 @@ export function Toolbar() {
 																	type="button"
 																	className="text-blue-400 hover:text-blue-300 border border-blue-400 hover:border-blue-300 text-[12px] px-4 py-2 rounded-[6px] font-medium transition-colors"
 																	onClick={() => {
-																		window.open("/pricing", "_blank");
+																		window.open("/settings/team", "_blank");
 																	}}
 																>
 																	Upgrade to Pro
