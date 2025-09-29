22f847629dd5515dbfb6c0d7083c7e7b55ae1fd9
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-09-29 11:29:13 +0900

    revert

--- apps/studio.giselles.ai/app/api/inngest/route.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "inngest/next";
-import { generateContent, inngest } from "../../../lib/inngest";
-
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-	client: inngest,
-	functions: [generateContent],
-});
